06e451ab1123d9fe2204493554497f77458633f1
dhruvinhp <dhruvinhprajapati@gmail.com>
2022-07-12 14:03:47 +0530

    feat: get book list graphql

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+
+// cors middleware
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://dhruvin:mongo123@cluster0.kazq6.mongodb.net/?retryWrites=true&w=majority"
